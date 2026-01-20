953051f6b7051a4b25dd386a2c65a3786258cd00
Sonam Dhadiwal <ssdhadiwal.it@gmail.com>
2026-01-20 19:20:28 +0530

    vertical matches disappearing

--- candy-crush-game/src/app/app.ts
+++ candy-crush-game/src/app/app.ts
@@ -23,9 +23,17 @@ export class App implements OnInit {
 
   gameBoard: string[] = [];
 
-  ngOnInit() {
-    this.createBoard();
-  }
+ ngOnInit() {
+  this.createBoard();
+
+  setInterval(() => {
+    this.checkRowOfThree();
+    this.checkColumnOfThree();
+    this.moveDown();
+  }, 150);
+}
+
+
 
   createBoard() {
     console.log("width", this.width);
@@ -38,4 +46,64 @@ export class App implements OnInit {
       this.gameBoard.push(randomColor);
     }
   }
+
+  checkRowOfThree() {
+  for (let i = 0; i < 64; i++) {
+    const row = [i, i + 1, i + 2];
+    const color = this.gameBoard[i];
+
+    const invalid = [
+      6, 7, 14, 15, 22, 23,
+      30, 31, 38, 39, 46, 47,
+      54, 55, 62, 63
+    ];
+
+    if (invalid.includes(i)) continue;
+
+    if (
+      row.every(
+        index =>
+          this.gameBoard[index] === color &&
+          color !== 'blank'
+      )
+    ) {
+      this.score += 3;
+      row.forEach(index => (this.gameBoard[index] = 'blank'));
+    }
+  }
+}
+
+moveDown() {
+  for (let i = 0; i < 56; i++) {
+    if (this.gameBoard[i + this.width] === 'blank') {
+      this.gameBoard[i + this.width] = this.gameBoard[i];
+      this.gameBoard[i] = 'blank';
+    }
+  }
+}
+
+checkColumnOfThree() {
+  for (let i = 0; i < 48; i++) {
+    const column = [
+      i,
+      i + this.width,
+      i + this.width * 2,
+    ];
+
+    const color = this.gameBoard[i];
+
+    if (
+      column.every(
+        index =>
+          this.gameBoard[index] === color &&
+          color !== 'blank'
+      )
+    ) {
+      this.score += 3;
+      column.forEach(index => (this.gameBoard[index] = 'blank'));
+    }
+  }
+}
+
+
 }
